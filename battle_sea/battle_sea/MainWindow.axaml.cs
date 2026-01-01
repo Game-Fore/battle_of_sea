a28cc3bdcd03e519fee6b5b7844e254b9a3613d0
polina607 <bondarchukpaulina@yandex.ru>
2026-01-01 21:46:01 +1000

    - create project

---
+++ battle_sea/battle_sea/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace battle_sea
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
